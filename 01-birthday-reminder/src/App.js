dca41cadbd6e825da148cdd3351aed3b4f892998
Joshua Morgan <jmorg0605@gmail.com>
2023-02-07 12:33:58 +0800

    completed exercise-added clear and reset functionality

--- 01-birthday-reminder/src/App.js
+++ 01-birthday-reminder/src/App.js
@@ -3,12 +3,23 @@ import data from './data';
 import List from './List';
 
 function App() {
+  const [people, setPeople] = useState(data);
+  
+  const clearAllPeople = () => {
+    setPeople([])
+  }
+
+  const resetPeople = () => {
+    setPeople(data);
+
+  }
   return (
     <main>
       <section className="container">
-        <h3>0 birthdays today</h3>
-        <List />
-        <button onClick={() => console.log('you clicked me')}>Clear All</button>
+        <h3>{people.length} birthdays today</h3>
+        <List people={people}/>
+        <button onClick={clearAllPeople}>Clear All</button>
+        <button type='button' onClick={resetPeople}>reset birthdays</button>
       </section>
     </main>
   );
